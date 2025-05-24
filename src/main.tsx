6b6085171182d4d378f3d1c33e2f97c9f460ab6a
Shadab Akram <shadabakram11@gmail.com>
2025-05-25 00:10:59 +0530

    Trying

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "remixicon/fonts/remixicon.css";
 
 const rootElement = document.getElementById("root");
 
